aee5640c03c0ea92d4144b0c3097735585f14b49
mufarreh <mufar.rey@gmail.com>
2024-04-26 17:36:40 +0600

    Questions and Answers Displayed

---
+++ src/components/Quiz.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import QUESTIONS from "../questions.js";
+
+export default function Quiz() {
+  const [userAnswers, setUserAnswers] = useState([]);
+
+  const activeQuestionIndex = userAnswers.length;
+
+  const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
+  shuffledAnswers.sort(() => Math.random() - 0.5);
+
+  function handleSelectAnswer(answer) {
+    setUserAnswers((prevAnswer) => {
+      return [...prevAnswer, answer];
+    });
+  }
+
+  return (
+    <div id="quiz">
+      <div id="question">
+        <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+        <ul id="answers">
+          {shuffledAnswers.map((answer) => (
+            <li key={answer} className="answer">
+              <button onClick={() => handleSelectAnswer(answer)}>
+                {answer}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
